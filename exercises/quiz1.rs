0f834a4177d248d007e4fab94073cd68decdee2b
LifeSteal <dorianosaure.pro@gmail.com>
2025-07-12 13:23:59 +0000

    quizz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number: i32) -> i32 {
+    if number > 40 {
+        return number;
+    }
+
+    return number*2;
+}
 
 // Don't modify this function!
 #[test]
@@ -31,3 +35,4 @@ fn verify_test() {
     assert_eq!(41, price3);
     assert_eq!(65, price4);
 }
+
